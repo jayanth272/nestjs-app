a82d2ffc69cb8c7eb221db1247f6cf3c0576780f
Jayanth <jayanth.b@hingehealth.com>
2023-08-08 15:05:55 +0530

    "Update product"

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -15,10 +15,30 @@ export class ProductsService {
         return [...this.products];
     }
     getSingleProduct(productId: string){
-        const product = this.products.find((prod)=> prod.id== productId);
+        const product = this.findProduct(productId)[0];// First index will have product second index will have productID
+        return {...product};
+    }
+    updateProduct(productId: string, title: string,desc: string,price: number)
+    {
+       const [product,index] = this.findProduct(productId);
+       const updatedProduct = {...product};
+       if(title){
+        updatedProduct.title = title;
+       }
+       if(desc){
+        updatedProduct.description = desc;
+       }
+       if(price){
+        updatedProduct.price = price ;
+       }
+       this.products[index]={...product,}
+    }
+    private findProduct(id: string): [Product,number]{
+        const productIndex = this.products.findIndex((prod)=> prod.id== id);
+        const product = this.products[productIndex];
         if(!product){
             throw new NotFoundException('Could not find the product');
         }
-        return {...product};
+        return [product,productIndex];
     }
 }
