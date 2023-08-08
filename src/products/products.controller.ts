765e1118de89ed520bebddc1feee65ecf1d338bf
Jayanth <jayanth.b@hingehealth.com>
2023-08-08 14:35:31 +0530

    "Get products, get single products"

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,4 @@
-import { Controller , Post, Body} from '@nestjs/common'
+import { Controller , Post, Body, Get, Param} from '@nestjs/common'
 import { ProductsService } from './products.service';
 
 @Controller('products')
@@ -19,5 +19,13 @@ constructor(private productsService : ProductsService){}
         );
         return {id: generatedId};
     }
-    
+    @Get()
+    getAllProducts(){
+        return this.productsService.getProducts();
+    }   
+    @Get(':id')
+    getProduct(@Param('id') prodId: string,){
+        return this.productsService.getSingleProduct(prodId);
+    }
+
 }
